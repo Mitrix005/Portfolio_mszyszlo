27ab4c2146266b6046b78d12d288b80855f246cb
Mitrix005 <michalszyszlo05@gmail.com>
2025-12-05 20:37:59 +0100

    Dodano content do Projects oraz zrobiono animacje

--- src/app/components/Cards/CardsWrapper.js
+++ src/app/components/Cards/CardsWrapper.js
@@ -1,24 +1,34 @@
 "use client"
-import { motion, useScroll, useTransform } from "motion/react"
-import { useRef } from "react";
+import { motion, useScroll, useTransform, useMotionValueEvent } from "motion/react"
+import {useRef, useState} from "react";
 import Skills from "@/app/components/Cards/Skills";
 import Projects from "@/app/components/Cards/Projects";
 
 export default function CardsWrapper() {
     const containerRef = useRef(null);
+    const [ProjectId, setProjectId] = useState(0);
 
     const { scrollYProgress } = useScroll({
         target: containerRef,
         offset: ["start start", "end end"]
     });
 
-    const x = useTransform(scrollYProgress, [0.3, 0.6], ["0%", "-100%"]);
-    const scale = useTransform(scrollYProgress,[0.3, 1], ["100%", "70%"]);
+    const x = useTransform(scrollYProgress, [0.3, 0.55], ["0%", "-100%"]);
+    const scale = useTransform(scrollYProgress,[0.3, 0.55], ["100%", "70%"]);
+    useMotionValueEvent(scrollYProgress, "change", (latest) => {
+        if (latest < 0.7) {
+            setProjectId(0);
+        } else if (latest < 0.85) {
+            setProjectId(1);
+        } else {
+            setProjectId(2);
+        }
+    });
 
     return (
         <div ref={containerRef} className="relative h-[400vh] bg-zinc-900 rounded-t-3xl">
             <div id="skills" className="absolute top-0 w-full h-1 pointer-events-none" />
-            <div id="projects" className="absolute top-[70%] w-full h-1 pointer-events-none" />
+            <div id="projects" className="absolute top-[40%] w-full h-1 pointer-events-none" />
 
             <div className="sticky top-0 h-screen overflow-hidden">
                 <div className="flex h-full w-[200vw]">
@@ -30,7 +40,7 @@ export default function CardsWrapper() {
                     <motion.div
                         style={{ x }}
                         className="relative z-20 w-screen h-full flex-shrink-0 flex items-center justify-center">
-                        <Projects />
+                        <Projects actualProject={ProjectId}/>
                     </motion.div>
                 </div>
             </div>
--- src/app/components/Cards/Projects.js
+++ src/app/components/Cards/Projects.js
@@ -1,49 +1,179 @@
 "use client"
 
-import {motion} from "motion/react"
+import {motion, AnimatePresence, useMotionValue, useSpring, useTransform} from "motion/react"
+import {useEffect} from "react";
+import Image from "next/image";
 
-export default function Projects(){
+export default function Projects({actualProject=0}){
+
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+
+    const xSpring = useSpring(x);
+    const ySpring = useSpring(y);
+
+    const rotateX = useTransform(ySpring, [-0.5, 0.5], ["9deg", "-9deg"]);
+    const rotateY = useTransform(xSpring, [-0.5, 0.5], ["-9deg", "9deg"]);
+
+
     const containerVariant = {
-        hidden: { opacity: 1 },
+        hidden: { opacity: 0 },
         visible: {
             opacity: 1,
             transition: {
-                staggerChildren: 0.3
+                staggerChildren: 0.1,
+                delayChildren: 0.1
             }
         }
     };
 
-    const projectsCardsVariant = {
-        hidden: { opacity: 0 },
+    const itemVariant = {
+        hidden: { opacity: 0, y: 20 },
         visible: {
             opacity: 1,
-            transition: { type: "spring"}
+            y: 0,
+            transition: { type: "spring", stiffness: 50 }
         }
     };
+
+    const projects = [
+        {
+            name: "HIGH SCHOOL OF JAN ZAMOYSKI IN ZAMOŚĆ",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis varius eros, eu fringilla lacus mollis semper. " +
+                "Integer dictum lacinia sapien quis luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+            preview: "",
+            color: "bg-[#FFR100]",
+            image: "/previews/1lo-preview.png",
+            link: "https://next-lo-ten.vercel.app"
+        },
+        {
+            name: "MY PORTFOLIO",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis varius eros, eu fringilla lacus mollis semper. " +
+                "Integer dictum lacinia sapien quis luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+            preview: "",
+            color: "bg-[#064e3b]",
+            link: "https://next-lo-ten.vercel.app"
+        },
+        {
+            name: "COMMING SOON",
+            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis varius eros, eu fringilla lacus mollis semper. " +
+                "Integer dictum lacinia sapien quis luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas",
+            preview: "",
+            color: "bg-[#6b21a8]",
+            link: "https://next-lo-ten.vercel.app"
+        }
+    ];
+    const projectData = projects[actualProject];
+
+    const contentVariant = {
+        hidden: { opacity: 0, x: 50, scale: 0.95 },
+        visible: {
+            opacity: 1,
+            x: 0,
+            scale: 1,
+            transition: { duration: 0.4, type: "spring", bounce: 0.3, staggerChildren: 0.1}
+        },
+        exit: {
+            opacity: 0,
+            x: -50,
+            scale: 0.95,
+            transition: { duration: 0.3 }
+        }
+    };
+
+
+    const mainContainerVariant = {
+        hidden: { opacity: 0, x: 20 },
+        visible: {
+            opacity: 1,
+            x: 0,
+            transition: { duration: 0.5, type: "spring", staggerChildren: 0.2 }
+        },
+        exit: { opacity: 0, x: -20, transition: { duration: 0.3 } }
+    };
+
+    const handleMouseMove = (e) => {
+        const rect = e.currentTarget.getBoundingClientRect();
+
+        const width = rect.width;
+        const height = rect.height;
+
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+
+        const xPrecent = mouseX / width - 0.5;
+        const yPrecent = mouseY / height - 0.5;
+
+        x.set(xPrecent);
+        y.set(yPrecent);
+    }
+
+    const handleMouseLeave = () => {
+        x.set(0);
+        y.set(0);
+    }
+
 
     return (
         <motion.div
             initial="hidden"
             whileInView="visible"
             variants={containerVariant}
-            viewport={{ once: false, amount: 0.8 }}
-            className="w-full h-full p-10 pr-12 bg-white">
-            <div className="relative w-full h-full">
-                <div className="absolute top-0 w-max h-max flex flex-col gap-4">
+            viewport={{ once: false, amount: 0.3 }}
+            className="w-full h-full p-10 pr-12 bg-neutral-100">
+            <motion.div
+                variants={itemVariant}
+                className="relative w-full h-full">
+                <div className="absolute top-0 w-max h-max flex flex-col gap-3">
                     <p className="font-inter text-5xl font-bold text-yellow-400">My projects</p>
                     <p className="text-zinc-900 text-sm font-light">Check out what I've made and what i can offer.</p>
                 </div>
-                <div className="w-full h-full flex justify-center items-center px-5 py-10 gap-5">
-                    <div className="w-2/5 h-3/4 flex flex-col justify-between items-end gap-2">
-                        <p className="relative flex-none top-0 text-zinc-600 text-8xl font-medium">#1</p>
-                        <div className="w-full h-max flex flex-col justify-end items-end gap-8">
-                            <h1 className="text-zinc-800 text-5xl/15 font-black text-right">HIGH SCHOOL OF JAN ZAMOYSKI IN ZAMOŚĆ</h1>
-                            <p className="text-zinc-900 text-lg font-medium text-right">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam lobortis varius eros, eu fringilla lacus mollis semper. Integer dictum lacinia sapien quis luctus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas</p>
+                <AnimatePresence mode="wait">
+                    <motion.div
+                        key={actualProject}
+                        variants={contentVariant}
+                        initial="hidden"
+                        animate="visible"
+                        exit="exit"
+                        className="w-full h-full flex justify-center items-center px-5 py-10 gap-8">
+                        <div className="w-2/5 h-3/4 flex flex-col justify-between items-end gap-2">
+                            <motion.p
+                                variants={itemVariant}
+                                className="relative flex-none top-0 text-zinc-600 text-8xl font-medium">#{actualProject+1}</motion.p>
+                            <div className="w-full h-max flex flex-col justify-end items-end gap-4">
+                                <motion.h1
+                                    variants={itemVariant}
+                                    className="text-zinc-600 text-5xl/16 font-black text-right">{projectData.name}</motion.h1>
+                                <motion.p
+                                    variants={itemVariant}
+                                    className="text-zinc-900 text-lg font-medium text-right">{projectData.description}</motion.p>
+                            </div>
                         </div>
-                    </div>
-                    <div className="w-3/5 h-3/4 bg-[#FFE100] rounded-2xl"></div>
-                </div>
-            </div>
+                        <motion.div
+                            className={`w-3/5 h-3/4 flex flex-col justify-end rounded-2xl shadow-xl/30 ${projectData.color} overflow-hidden relative border border-zinc-300`}
+                            whileHover={{scale: 1.01, transition:{
+                                duration: 0.2,
+                                }}}
+                            onMouseMove={handleMouseMove}
+                            onMouseLeave={handleMouseLeave}
+                            style={{
+                                rotateX,
+                                rotateY,
+                                transformStyle: "preserve-3d"
+                            }}
+                        >
+                            {projectData?.image ? <div className="absolute top-0 left-0 w-full h-full">
+                                <Image src={projectData.image} alt={projectData.name} fill className="object-cover" />
+                            </div> : null}
+                            <div className="relative z-10 w-full h-12 bg-neutral-300 rounded-b-2xl flex items-center justify-center px-4">
+                                <div className="w-max h-max bg-white rounded-2xl flex items-center pl-3 pr-6 py-1">
+                                    <p className="text-sm text-zinc-800">{projectData.link}</p>
+                                </div>
+                            </div>
+                        </motion.div>
+                    </motion.div>
+                </AnimatePresence>
+            </motion.div>
         </motion.div>
     )
 }
